7fa38bbc1fc11704c6cfb33aa5737c76d072e0cd
csi0n <huaqing.chen@bioon.com>
2016-09-21 14:43:42 +0800

    Done

--- src/ExceptionEmailRepository.php
+++ src/ExceptionEmailRepository.php
@@ -16,34 +16,39 @@ class ExceptionEmailRepository
     protected $name;
     protected $subject;
     protected $ignore;
+    protected $enable;
+
     /**
      * ExceptionEmailRepository constructor.
      */
     public function __construct($app)
     {
         $this->app = $app;
-        $this->email=config('exception_mail.email');
-        $this->name=config('exception_mail.name');
-        $this->subject=config('exception_mail.subject');
-        $this->ignore=config('exception_mail.ignore');
+        $this->email = config('exception_mail.email');
+        $this->name = config('exception_mail.name');
+        $this->subject = config('exception_mail.subject');
+        $this->ignore = config('exception_mail.ignore');
+        $this->enable = config('exception_mail.enable');
     }
 
     public function send($e)
     {
-      if (!empty($this->email)&&!empty($this->name)) {
-        $send=false;
-        foreach ($this->ignore as $k) {
-          if ($k instanceof $e) {
-            $send=true;
-          }
+        if (!$this->enable)
+            return false;
+        if (!empty($this->email) && !empty($this->name)) {
+            $send = true;
+            foreach ($this->ignore as $k) {
+                if ($e instanceof $k) {
+                    $send = false;
+                }
+            }
+            if ($send) {
+                Mail::send('vendor.csi0n.mail.exception', ['exception' => $e], function ($message) {
+                    $message->to($this->email, $this->name)->subject($this->subject);
+                });
+                return true;
+            }
         }
-        if ($send) {
-          Mail::send('vendor.csi0n.mail.exception', ['exception'=>$e], function ($message){
-              $message->to($this->email, $this->name)->subject($this->subject);
-          });
-          return true;
-        }
-      }
-      return false;
+        return false;
     }
 }
--- src/config/exception_mail.php
+++ src/config/exception_mail.php
@@ -3,12 +3,12 @@
 return [
     'enable'=>true,
     /*to email*/
-    'email'=>'chqssqll@gmail.com',
-    /*to name*/,
+    'email'=>'huaqing.chen@bioon.com',
+    /*to name*/
     'name'=>'Exception Email',
     'subject'=>'Exception Email Subject',
     /*some of you want to ignore exception*/
     'ignore'=>[
-      Symfony\Component\HttpKernel\Exception\NotFoundHttpException,
+        Symfony\Component\HttpKernel\Exception\NotFoundHttpException::class,
     ],
 ];
